e04c24d78232dc680c5a8bf83afd25fef30b6318
Paul Tsviliuk <paultsvilyuk@gmail.com>
2023-07-02 13:50:52 -0700

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+def insert(array, rightIndex, value):
+  j = rightIndex
+  while j >= 0 and array[j] > value:
+    array[j + 1] = array[j]
+    j = j - 1
+  array[j + 1] = value
+
+  
+def insertionSort(array):
+  for i in range(1, len(array)):
+    insert(array, i-1, array[i])
+  return array
